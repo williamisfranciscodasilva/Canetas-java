99ba2a7267fb3a0738e7640db1e685589b6b2e91
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-01-23 18:06:30 -0200

    Aplicando o conceito de métodos acessores, modificadores e construtores ao projeto

--- src/canetas/Caneta.java
+++ src/canetas/Caneta.java
@@ -5,17 +5,53 @@ import javax.swing.JOptionPane;
 public class Caneta {
 	public String modelo, cor;
 	private float ponta;
-	protected int carga;
+	protected int tubo;
 	private boolean tampada;
 	
+	public Caneta(String m, String c, float p, int t) {
+		this.modelo =m;
+		this.cor =c ;
+		this.ponta = p;
+		this.tubo = t;
+		
+	}
+	
+	public String getModelo() {
+		return this.modelo;
+	}
+	public void setModelo(String m) {
+		this.modelo = m;
+	}
+	public String getCor() {
+		return this.cor;
+	}
+	public void setCor(String c) {
+		this.cor = c;
+	}
+	
+	public float getPonta() {
+		return this.ponta;
+	}
+	public void setPonta(float p) {
+		this.ponta = p;
+	}
+	public int getTubo() {
+		return this.tubo;
+	}
+	public void setTubo(int t) {
+		this.tubo = t;
+	}
+	
 	public void status() {
-		JOptionPane.showMessageDialog(null,"Modelo "+this.modelo+"\nUma caneta "+ this.cor+
-				"\nPonta "+this.ponta+"\nCarga "+this.carga+"\nEstá tampada? "+ this.tampada);
-		System.out.println("Modelo "+this.modelo);
-		System.out.println("Uma caneta "+ this.cor);
-		System.out.println("Ponta "+this.ponta);
-		System.out.println("Carga "+this.carga);
-		System.out.println("Está tampada? "+ this.tampada);
+		
+		JOptionPane.showMessageDialog(null,"Sobre a Caneta: "+"\nModelo "+this.modelo);
+		System.out.println("Sobre a Caneta: ");
+		System.out.println("Modelo "+this.getModelo());
+		System.out.println("Ponta "+this.getPonta());
+		System.out.println("Tubo "+this.getTubo());
+		System.out.println("Cor "+this.cor);
+		System.out.println("Tampada "+this.tampada);
+		
 	}
 	
 	public void rabiscar() {
@@ -36,4 +72,5 @@ public class Caneta {
 	public void destampar() {
 		this.tampada =false;
 	}
+	
 }
--- src/canetas/MainProgram.java
+++ src/canetas/MainProgram.java
@@ -4,24 +4,13 @@ public class MainProgram {
 
 	public static void main(String[] args) {
 		
-		Caneta c1 = new Caneta();
-		c1.modelo = "Bic cristal";
-		c1.cor = "Azul";
-		//c1.ponta = 0.5f;
-		c1.carga = 80;
-		//c1.tampada = true;
-		//c1.tampar();
-		
+		Caneta c1 = new Caneta("NIC", "Amarelo", 0.4f, 20);
 		c1.status();
+		c1.tampar();
 		c1.rabiscar();
-		c1.destampar();
-		
-		Caneta c2 = new Caneta();
-		c2.modelo = "Bic normal";
-		c2.cor = "Preta";
-		c2.destampar();
-		
+		Caneta c2 = new Caneta("VIC", "Laranja", 1.2f, 15);
 		c2.status();
+		c2.tampar();
 		c2.rabiscar();
 	}
 
